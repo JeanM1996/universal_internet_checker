1727cb3a249d6addffdd7d3988afa183fafe8332
Isuru Walpola <iwalpola@gmail.com>
2021-09-17 23:14:09 +0530

    use http package for multiplatform

--- lib/universal_internet_checker.dart
+++ lib/universal_internet_checker.dart
@@ -1,15 +1,12 @@
 library universal_internet_checker;
 
 import 'dart:async';
-import 'dart:io';
+import 'package:http/http.dart' as http;
 
 class UniversalInternetChecker {
-  static final Uri google = Uri.parse('https://dns.google.com/resolve');
-  static final Uri cloudflare =
-      Uri.parse('https://cloudflare-dns.com/dns-query');
-  static final Uri quad9 = Uri.parse('https://dns.quad9.net:5053/dns-query');
+  static Uri doHService = DoHServices.cloudflare;
 
-  static Duration _interval = Duration(milliseconds: 1500);
+  static Duration _interval = Duration(milliseconds: 2000);
 
   //the domain to ask dns for
   static String _domain = 'google.com';
@@ -18,8 +15,7 @@ class UniversalInternetChecker {
   bool? _lastStatus;
 
   /// Timer to check periodically the internet status
-  Timer? _timer;
-  //TODO: should this timer be late?
+  late Timer _timer;
 
   /// Stream controller to emit a notifier when connection status changes
   final StreamController<bool> _streamController =
@@ -40,18 +36,12 @@ class UniversalInternetChecker {
     _streamController.onListen = _setupPolling;
 
     _streamController.onCancel = () {
-      _timer?.cancel();
+      _timer.cancel();
       _lastStatus = null;
     };
   }
 
-  /// Method to set the lookup address
-  // void setLookUpAddress(String? lookUpAddress) {
-  //   _lookUpAddress = lookUpAddress;
-  // }
-
   void _setupPolling() {
-    _timer?.cancel();
     _checkAndBroadcast();
     _timer = Timer.periodic(_interval, _checkAndBroadcast);
   }
@@ -62,10 +52,10 @@ class UniversalInternetChecker {
 
     bool isConnected = await checkInternet();
 
-    print('connection status: $isConnected');
-
-    if (_lastStatus != isConnected && _streamController.hasListener)
+    if (_lastStatus != isConnected && _streamController.hasListener) {
       _streamController.add(isConnected);
+      print('connection status changed: $isConnected');
+    }
 
     _lastStatus = isConnected;
   }
@@ -74,23 +64,28 @@ class UniversalInternetChecker {
   /// lookUpAddress: String to use as lookup address to check internet connection
   static Future<bool> checkInternet() async {
     try {
-      var client = HttpClient();
-      // Set HttpClient timeout
-      client.connectionTimeout = Duration(milliseconds: 1200);
       // Init request query parameters and send request
-      var request = await client.getUrl(cloudflare.replace(
-          queryParameters: {'name': _domain, 'type': 'A', 'dnssec': '1'}));
-      // Set request http header (need for 'cloudflare' provider)
-      request.headers.add('Accept', 'application/dns-json');
+      http.Response response = await http.get(
+          doHService.replace(
+              queryParameters: {'name': _domain, 'type': 'A', 'dnssec': '1'}),
+          headers: {
+            'Accept': 'application/dns-json'
+          }).timeout(Duration(milliseconds: 1200));
+
       // Close & retrive response
-      var response = await request.close();
       if (response.statusCode == 200) {
         return true;
       }
       return false;
     } catch (e) {
-      print('GET Error (probably no internet)');
       return false;
     }
   }
+}
+
+class DoHServices {
+  static final Uri google = Uri.parse('https://dns.google.com/resolve');
+  static final Uri cloudflare =
+      Uri.parse('https://cloudflare-dns.com/dns-query');
+  static final Uri quad9 = Uri.parse('https://dns.quad9.net:5053/dns-query');
 }
